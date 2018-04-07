8644706382c7151a871380d39a8c4409364fafa3
Unknown <shukur.aydarov@gmail.com>
2018-04-07 19:46:00 +0700

    Webpack config added.

---
+++ src/FileServer/ClientApp/webpack.config.js
@@ -0,0 +1,22 @@
+﻿var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: './src//main.js',
+    output: {
+        path: path.resolve(__dirname, '../wwwroot/dist'),
+        publicPath: '/dist',
+        filename:'scripts.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ]
+}
